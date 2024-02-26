4ba27ac1553e41c1cfc8737f34371a2f4e08350d
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-02-26 20:38:01 +0530

    Dynamic and static Fragments

--- app/src/main/java/com/example/appc/MainActivity.java
+++ app/src/main/java/com/example/appc/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft=fm.beginTransaction();
+
+        BlankFragment2 bf=new BlankFragment2();
+        ft.add(R.id.fragmentContainerView,bf,null);
+        ft.commit();
+
+
     }
 }
